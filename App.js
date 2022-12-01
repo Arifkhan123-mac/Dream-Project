c6d08d5d79038b91e887b20d4b450043a0673f3a
Arifkhan123-mac <112231263+Arifkhan123-mac@users.noreply.github.com>
2022-12-01 16:38:27 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+//import logo from './logo.svg';
+//import './App.css';
+
+function App() {
+  return(
+    <>
+    <h1  data-ns-test attribute="project-name">Dream Project</h1>
+    <p  data-ns-test attribute="project-description">i want to make a website for my friend</p>
+    </>
+  )
+}
+
+export default App;
